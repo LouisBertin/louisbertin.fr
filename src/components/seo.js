7ffb3aec328d9e4a06146c53ee353319d6d4b188
LouisBertin <bertin.louis7@gmail.com>
2020-02-01 22:15:46 +0400

    fix missing import

--- src/components/seo.js
+++ src/components/seo.js
@@ -6,9 +6,8 @@
  */
 
 import React from "react"
-import PropTypes from "prop-types"
 import Helmet from "react-helmet"
-import { useStaticQuery, graphql } from "gatsby"
+import { StaticQuery, query } from "gatsby"
 
 const SEO = ({ title, description, image, pathname, article }) => (
   <StaticQuery
